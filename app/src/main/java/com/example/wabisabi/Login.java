a5ce32cfcef44dbf2fb2e150cd827340c399668e
Krishna Goswami <114379355+krishnaGauss@users.noreply.github.com>
2023-09-13 11:10:12 +0530

    Sign in UI

--- app/src/main/java/com/example/wabisabi/Login.java
+++ app/src/main/java/com/example/wabisabi/Login.java
@@ -2,13 +2,25 @@ package com.example.wabisabi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
