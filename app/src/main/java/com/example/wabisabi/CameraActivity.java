b5b7390bdb42ca7d9cef637b9d25c0d37fb73e62
AtharvVerma-290 <atharv.verma29k@gmail.com>
2023-09-16 02:16:14 +0530

    NavBar working

--- app/src/main/java/com/example/wabisabi/CameraActivity.java
+++ app/src/main/java/com/example/wabisabi/CameraActivity.java
@@ -1,40 +1,38 @@
 package com.example.wabisabi;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class Login extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        bottomNavigationView = findViewById(R.id.bottomNav);
+        setContentView(R.layout.activity_camera);
 
+        bottomNavigationView = findViewById(R.id.bottomNav2);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
                 if (itemId == R.id.bottom_home) {
+                    startActivity(new Intent(CameraActivity.this, Login.class));
                     return true;
                 } else if (itemId == R.id.bottom_camera) {
-                    startActivity(new Intent(Login.this, CameraActivity.class));
                     return true;
                 } else if (itemId == R.id.bottom_profile) {
-                    startActivity(new Intent(Login.this, ProfileAct.class));
+                    startActivity(new Intent(CameraActivity.this, ProfileAct.class));
                     return true;
                 }
                 return false;
             }
         });
-
     }
 }
