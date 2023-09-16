77c09ae9c2e506afb9127285855a36dac6c28e8a
Harshvardhansingh19 <137829610+Harshvardhansingh19@users.noreply.github.com>
2023-09-16 10:49:43 +0530

    login signin database added

--- app/src/main/java/com/example/wabisabi/Login.java
+++ app/src/main/java/com/example/wabisabi/Login.java
@@ -6,12 +6,12 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
-    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        button = findViewById(R.id.button4);
+
     }
 }
