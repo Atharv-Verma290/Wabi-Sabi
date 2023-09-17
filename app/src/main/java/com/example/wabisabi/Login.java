c773a7438069d4e505dafeff5a2ead1a4ba6fc82
AtharvVerma-290 <atharv.verma29k@gmail.com>
2023-09-17 21:46:52 +0530

    Padding added in input and info fields

--- app/src/main/java/com/example/wabisabi/Login.java
+++ app/src/main/java/com/example/wabisabi/Login.java
@@ -2,15 +2,11 @@ package com.example.wabisabi;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class Login extends AppCompatActivity {
 
@@ -26,29 +22,23 @@ public class Login extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNav);
         quizstart = findViewById(R.id.imageView19);
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.bottom_home) {
-                    return true;
-                } else if (itemId == R.id.bottom_camera) {
-                    startActivity(new Intent(Login.this, CameraActivity.class));
-                    return true;
-                } else if (itemId == R.id.bottom_profile) {
-                    startActivity(new Intent(Login.this, ProfileAct.class));
-                    return true;
-                }
-                return false;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.bottom_home) {
+                return true;
+            } else if (itemId == R.id.bottom_camera) {
+                startActivity(new Intent(Login.this, CameraActivity.class));
+                return true;
+            } else if (itemId == R.id.bottom_profile) {
+                startActivity(new Intent(Login.this, ProfileAct.class));
+                return true;
             }
+            return false;
         });
 
-        quizstart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, com.example.wabisabi.quizstart.class);
-                startActivity(intent);
-            }
+        quizstart.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, com.example.wabisabi.quizstart.class);
+            startActivity(intent);
         });
 
     }
