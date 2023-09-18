415735638b82e387d74e7c8d5608343ab0da4715
AtharvVerma-290 <atharv.verma29k@gmail.com>
2023-09-18 19:54:19 +0530

    quiz options

--- app/src/main/java/com/example/wabisabi/quizsecond.java
+++ app/src/main/java/com/example/wabisabi/quizsecond.java
@@ -3,17 +3,23 @@ package com.example.wabisabi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class quizsecond extends AppCompatActivity {
-    Button button;
+public class quizsecond extends AppCompatActivity implements View.OnClickListener{
+    Button button,Option1,Option2,Option3,Option4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizsecond);
         button=findViewById(R.id.buttonquiz);
+        Option1 = findViewById(R.id.option_1);
+        Option2 = findViewById(R.id.option_2);
+        Option3 = findViewById(R.id.option_3);
+        Option4 = findViewById(R.id.option_4);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +27,19 @@ public class quizsecond extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Option1.setOnClickListener(this);
+        Option2.setOnClickListener(this);
+        Option3.setOnClickListener(this);
+        Option4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button clickedButton = (Button) v;
+        if (clickedButton.getId() == R.id.option_2){
+            clickedButton.setBackgroundColor(Color.GREEN);
+        }else{
+            clickedButton.setBackgroundColor(Color.RED);
+        }
     }
 }
