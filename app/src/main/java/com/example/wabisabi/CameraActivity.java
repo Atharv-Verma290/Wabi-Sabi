fb874feeef78a647009982e5bb7ba1369c999bef
AtharvVerma-290 <atharv.verma29k@gmail.com>
2023-09-18 22:08:21 +0530

    Icon added

--- app/src/main/java/com/example/wabisabi/CameraActivity.java
+++ app/src/main/java/com/example/wabisabi/CameraActivity.java
@@ -1,17 +1,18 @@
 package com.example.wabisabi;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class CameraActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
