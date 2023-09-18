c1a83a2a3b91d03494feea32486e1a76617e494d
AtharvVerma-290 <atharv.verma29k@gmail.com>
2023-09-19 02:28:01 +0530

    Final prototype finished

--- app/src/main/java/com/example/wabisabi/MainActivity.java
+++ app/src/main/java/com/example/wabisabi/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.wabisabi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,7 +15,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     EditText signupName;
-
     EditText signupphonenumber;
     EditText signuppassword;
     EditText signuprecreatepassword;
@@ -49,32 +47,26 @@ public class MainActivity extends AppCompatActivity {
         joinUs = findViewById(R.id.button);
         signUp = findViewById(R.id.button2);
 
-        joinUs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
+        joinUs.setOnClickListener(view -> {
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("users");
 
-                String name = signupName.getText().toString();
-                String phone_number = signupphonenumber.getText().toString();
-                String  create_pass = signuppassword .getText().toString();
-                String re_enter_pa = signuprecreatepassword.getText().toString();
+            String name = signupName.getText().toString();
+            String phone_number = signupphonenumber.getText().toString();
+            String  create_pass = signuppassword .getText().toString();
+            String re_enter_pa = signuprecreatepassword.getText().toString();
 
-                HelperClass helperClass = new HelperClass(name, phone_number,  create_pass, re_enter_pa);
-                reference.child(name).setValue(helperClass);
+            HelperClass helperClass = new HelperClass(name, phone_number,  create_pass, re_enter_pa);
+            reference.child(name).setValue(helperClass);
 
-                Toast.makeText(MainActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
+            Toast.makeText(MainActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
         });
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SignupActivity.class);
-                startActivity(intent);
-            }
+        signUp.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,SignupActivity.class);
+            startActivity(intent);
         });
 
     }
